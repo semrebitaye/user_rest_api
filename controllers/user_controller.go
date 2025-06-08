9f420593449a36e55fdd9fabcac964978f59e61e
semrebitaye <asimrobitaye23@gmail.com>
2025-06-08 14:50:18 +0300

    hash password utility added

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -9,6 +9,7 @@ import (
 	"user_rest_api/models"
 
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Create user
@@ -19,21 +20,35 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	db := initializer.ConnectToDb()
 	defer db.Close()
 
+	var body struct {
+		FirstName string
+		LastName  string
+		UserName  string
+		Password  string
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
 	// create user istance
-	user := &models.User{}
+	user := &models.User{FirstName: body.FirstName, LastName: body.LastName, UserName: body.UserName, Password: string(hash)}
 
 	// decoding the request
 	_ = json.NewDecoder(r.Body).Decode(&user)
 
 	// inserting into database
-	_, err := db.Model(user).Insert()
+	result, err := db.Model(user).Insert()
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	// returning user
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(result)
 
 }
 
