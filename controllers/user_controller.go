903dc066ddc233668120ebfc4d24dcc345488b15
semrebitaye <asimrobitaye23@gmail.com>
2025-06-09 07:33:19 +0300

    login added

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -4,8 +4,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 	"user_rest_api/initializer"
 	"user_rest_api/models"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // Create user
@@ -16,20 +23,184 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	db := initializer.ConnectToDb()
 	defer db.Close()
 
+	var body struct {
+		FirstName string
+		LastName  string
+		UserName  string
+		Password  string
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
 	// create user istance
-	user := &models.User{}
+	user := &models.User{FirstName: body.FirstName, LastName: body.LastName, UserName: body.UserName, Password: string(hash)}
 
 	// decoding the request
 	_ = json.NewDecoder(r.Body).Decode(&user)
 
 	// inserting into database
-	_, err := db.Model(user).Insert()
+	result, err := db.Model(user).Insert()
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 	}
 
 	// returning user
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(result)
 
+}
+
+// get all users from the database
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// connect to db
+	db := initializer.ConnectToDb()
+	defer db.Close()
+
+	// creating user slice and select all from the database
+	var users []models.User
+	if err := db.Model(&users).Select(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// returning product
+	json.NewEncoder(w).Encode(users)
+}
+
+// get single user with pk
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "json/application")
+
+	// connect to db
+	db := initializer.ConnectToDb()
+	defer db.Close()
+
+	// get id
+	params := mux.Vars(r)
+	userId := params["id"]
+	userID, err := strconv.Atoi(userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get the user by the id
+	user := &models.User{ID: userID}
+	if err := db.Model(user).WherePK().Select(); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// return product
+	json.NewEncoder(w).Encode(user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "json/application")
+
+	// connect to db
+	db := initializer.ConnectToDb()
+	defer db.Close()
+
+	// get id of url
+	params := mux.Vars(r)
+	userId := params["id"]
+
+	// get the user based on the req id and delete it
+	product := &models.User{}
+	result, err := db.Model(product).Where("id = ?", userId).Delete()
+	// or we can use
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// return result
+	json.NewEncoder(w).Encode(result)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "json/application")
+
+	// connect to db
+	db := initializer.ConnectToDb()
+	defer db.Close()
+
+	// get the id of url
+	params := mux.Vars(r)
+	userId := params["id"]
+	userID, err := strconv.Atoi(userId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get the user and update based of the id of the req
+	user := &models.User{ID: userID}
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	_, err = db.Model(user).WherePK().Set("first_name = ?, last_name = ?, user_name = ?, password = ?", user.FirstName, user.LastName, user.UserName, user.Password).Update()
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// connect to db
+	db := initializer.ConnectToDb()
+	defer db.Close()
+
+	var body struct {
+		Username string
+		Password string
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var user models.User
+	db.Model(&user).First()
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// generate a jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("secret")))
+
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(tokenString)
 }
--- go.mod
+++ go.mod
@@ -6,15 +6,16 @@ toolchain go1.23.10
 
 require (
 	github.com/go-pg/pg v8.0.7+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.27.0
 )
 
 require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.37.0 // indirect
-	golang.org/x/crypto v0.27.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	mellium.im/sasl v0.3.2 // indirect
 )
